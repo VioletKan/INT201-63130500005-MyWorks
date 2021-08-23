2e29c57e8218b5f1fa716a3e104065294f70d977
Kanjanaporn <yokkanjanaporn@gmail.com>
2021-08-23 23:33:51 +0700

    selflearn

---
+++ SelfLearn/borntodev.js
@@ -0,0 +1,58 @@
+  
+
+        console.log("Hello" + " " + "World" )
+        var name = "a" + "bc"
+        console.log(name)
+        name = "Yok"
+        console.log(name)
+        var firstname = "Kanjanporn"
+        console.log(firstname)
+        var lastname = "Chuenmaneerat"
+        console.log(lastname)
+
+        var age = 12
+        console.log((4+2*2/2*2+4+1-5+19)+91)
+        console.log(typeof true)  
+
+        console.log("Hello" + " " + "World" )
+        var name = "a" + "bc"
+        console.log(name)
+        name = "Yok"
+        console.log(name)
+        var firstname = "Kanjanporn"
+        console.log(firstname)
+        var lastname = "Chuenmaneerat"
+        console.log(lastname)
+
+        alert("Hello")
+        var age = 12
+        console.log((4+2*2/2*2+4+1-5+19)+91)
+        console.log(typeof true)
+
+        var x = 200
+        {
+            var x = 20
+            let y = 15
+        }
+        console.log(x)
+
+        let lottonum = prompt("Enter your number :")
+        document.getElementById("bought").innerHTML = lottonum
+        document.getElementById("result").innerHTML = Math.floor(Math.random()*100)
+
+        console.log(1 == 1)
+        console.log(4 == "4")
+        console.log(5 == 6)
+
+        console.log("3" === "3")
+        console.log("Hello" == "Hello")
+        console.log(4 ==="4")
+        console.log(4 !== "4")
+
+        console.log(10 > 5 )
+
+        let age = prompt("Enter your age")
+        if (age > 13) {
+            console.log("Hello")
+        }
+
